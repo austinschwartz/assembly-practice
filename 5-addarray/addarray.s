74a5f9072832f3a304fa5bb8c04a17145d3665f7
Austin Schwartz <schwar12@purdue.edu>
2013-10-18 09:17:04 -0400

    added comments

--- 5-addarray/addarray.s
+++ 5-addarray/addarray.s
@@ -9,24 +9,25 @@ addarray:
 	mov	r3, #0
 
 while:
-	cmp	r3, r0
+	cmp	r3, r0 /* if (r3 == 0) return
 	beq	endsum
 
-	mov	r4, r3
+	mov	r4, r3 
 	mov	r5, #4
 	mul	r4, r4, r5
 	add	r4, r1, r4
-	ldr	r4,[r4]
+	ldr	r4,[r4] /* r4 = *(a + (i * 4)) */
 
-	add	r2, r2, r4
+	add	r2, r2, r4 /* sum += r4 */
 
 	mov	r5, #1
-	add	r3, r3, r5
+	add	r3, r3, r5 /* i += 1 */
 
 	b	while
 
 endsum:
 	mov	r0, r2
+	b	printf	/* print sum */
 
 	ldmfd	sp!, {r4, r5, fp, pc}
 
